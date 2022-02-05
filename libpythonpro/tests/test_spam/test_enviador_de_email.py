8805ddb9d881df637dc48f69ae2de942d3e05317
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-05 03:59:41 -0300

    Teste de Exceções

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,6 +1,6 @@
 import pytest
 
-from libpythonpro.spam.enviador_de_email import Enviador
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
@@ -20,3 +20,18 @@ def test_remetente(destinatario):
         'Turma Moacir Modas aberta'
     )
     assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+['', 'jonathan']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'luciano@python.pro.br',
+            'Cursos Python Pro',
+            'Turma Moacir Modas aberta'
+        )
