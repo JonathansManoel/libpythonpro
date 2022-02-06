786083de2877c82a5a8e626a8ee9f28c4f1a2445
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-06 03:01:40 -0300

    Produção de Código Testável

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,12 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Jonathan', email='jonathan@python.pro.br'),
+            Usuario(nome='Luciano', email='luciano@python.pro.br')
+        ],
+        [
+            Usuario(nome='Jonathan', email='jonathan@python.pro.br')
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'jonathan@python.pro.br',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
-
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Jonathan')
+    usuario = Usuario(nome='Jonathan', email='jonathan@python.pro.br')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Jonathan'), Usuario(nome='Luciano')]
+    usuarios = [
+        Usuario(nome='Jonathan', email='jonathan@python.pro.br'),
+        Usuario(nome='Luciano', email='jonathan@python.pro.br')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
