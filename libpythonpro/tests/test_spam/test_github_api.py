fd8f17d9f037c79bd024cc6b81136014320a3c4d
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-06 06:14:05 -0300

    Biblioteca Pytest-Mock

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -23,9 +23,9 @@ def test_buscar_avatar(avatar_url):
     assert avatar_url == url
 
 
-def test_buscar_avatar_integracao(avatar_url):
-    url = github_api.buscar_avatar('jonathansmanoel')
-    assert 'https://avatars.githubusercontent.com/u/96275361?v=4' == url
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzon')
+    assert 'https://avatars.githubusercontent.com/u/3457115?v=4' == url
 
 
 
